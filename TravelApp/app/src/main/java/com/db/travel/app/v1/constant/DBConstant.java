2c85d7e5a48539cad41cdd51224f3bce541b97cf
NupurB <nupurb08@gmail.com>
2015-11-13 16:38:06 -0500

    Initial Project structure

---
+++ TravelApp/app/src/main/java/com/db/travel/app/v1/constant/DBConstant.java
@@ -0,0 +1,15 @@
+package com.db.travel.app.v1.constant;
+
+/**
+ * Constants related to database file
+ * Such as file path and file name
+ */
+public abstract class DBConstant
+{
+    //database file directory
+    public static String DATABASE_PATH = "/data/data/com.db.travel.app/databases";
+    //database file name
+    public static String DATABASE_FILE = "library.db";
+    //database version
+    public static int DATABASE_VERSION = 1;
+}
---
+++ TravelApp/app/src/main/java/com/db/travel/app/v1/constant/SQLCommand.java
@@ -0,0 +1,7 @@
+package com.db.travel.app.v1.constant;
+
+public abstract class SQLCommand
+{
+    public static String QUERY_1 = "select dest_name from destination;";
+
+}
